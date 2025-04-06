c1f798186b490543321c37e1a9f93cd7dab182aa
SharkyBoy <sharkyboycoin@gmail.com>
2025-04-05 20:49:01 -0400

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,22 +8,25 @@ const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.Top}>
-        <div></div>
         <div className={styles.LinkContainer}>
-          <a href="https://sharkyboy.com" target="_blank">
+          <a href="https://sharkyboy.com" target="_blank" rel="noopener noreferrer">
             Home
           </a>
           <a href="/">Mint</a>
         </div>
-        <WalletMultiButton
-          style={{
-            background:
-              "linear-gradient(108deg, rgba(212,166,227,1) 0%, rgba(102,182,195,1) 100%)",
-            color: "var(--c3)",
-            border: "4px solid var(--c5)",
-            borderRadius: "10px",
-          }}
-        />
+        <div className={styles.WalletWrapper}>
+          <WalletMultiButton
+            style={{
+              background:
+                "linear-gradient(108deg, rgba(212,166,227,1) 0%, rgba(102,182,195,1) 100%)",
+              color: "var(--c3)",
+              border: "4px solid var(--c5)",
+              borderRadius: "10px",
+              fontFamily: "Orbitron, sans-serif",
+              fontWeight: "bold",
+            }}
+          />
+        </div>
       </div>
     </div>
   );
